e352ebb86f3d297caf5cf399ae17f8ba172485f7
MASUMHD <mrmasum2k4@gmail.com>
2024-05-11 00:15:08 +0600

    all data show code push

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,6 +28,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const newsBlogCollection = client.db("sportsNews").collection("newsBlog");
+
+    // data add
+    app.post('/addBlogs', async (req, res) => {
+        const newBlog = req.body;
+        console.log(newBlog);
+        const result = await newsBlogCollection.insertOne(newBlog);
+        res.send(result)
+    })
+
+    // data show
+    app.get('/allBlogs', async (req, res) => {
+        const cursor = newsBlogCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    // one data show
+    app.get('/allBlogs/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await newsBlogCollection.findOne(query);
+        res.send(result)
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
