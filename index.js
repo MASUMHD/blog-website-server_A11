9d8bee0a1dbfc0db82350597c90d53e229043436
MASUMHD <mrmasum2k4@gmail.com>
2024-05-14 17:09:55 +0600

    jwt code push

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -24,6 +32,29 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middlewares
+const logger = (req, res, next) => {
+  console.log('log: info',req.method, req.url);
+  next();
+}
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  // console.log('token in the middlewares:', token);
+  if(!token){
+    return res.status(401).send({ message: 'unauthorized access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({ message: 'unauthorized access'})
+    }
+    req.user = decoded;
+    next()
+  })
+
+}
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -35,6 +66,31 @@ async function run() {
     await newsBlogCollection.createIndex({ title: 'text', short_description: 'text' })
     // comment
     const commentCollection = client.db("sportsNews").collection("comments");
+
+
+
+    // auth related api
+    app.post('/jwt', logger, async(req,res)=>{
+      const user = req.body;
+      console.log('user for token:',user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+      .send({success: true})
+    })
+
+    // log out
+    app.post('/logout', async(req, res) => {
+      const user = req.body;
+      console.log('user for logout:',user);
+      res.clearCookie('token', {maxAge: 0}).send({success: true})
+    })
+
+
 
 
     // comment add
@@ -123,8 +179,14 @@ async function run() {
     })
 
     // show wishlist data and email query
-    app.get('/wishlist', async (req, res) => {
+    app.get('/wishlist', logger,verifyToken, async (req, res) => {
         // console.log(req.query.email);
+        console.log('token owner infooooo',req.user);
+
+        if(req.user.email !== req.query.email){
+          return res.status(403).send({ message: 'forbidden access' })
+        }
+
         let query = {};
         if (req.query.email) {
           query = { email: req.query.email }
